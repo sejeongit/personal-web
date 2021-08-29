75dfb5a069177c5e7936731e1ec6632005b1c90b
Sejeong Kim <83333195+sejeongit@users.noreply.github.com>
2021-08-29 19:32:56 +0900

    parallax 기능 수정2

--- js/src/rocket_parallax.js
+++ js/src/rocket_parallax.js
@@ -17,20 +17,21 @@ var rocket = intro.find('.rocket');
 win.on('scroll', function(e){
   var winScroll = win.scrollTop();
   var winH = win.outerHeight();
-  var rocTop, findScroll, percentScroll, per;
+  var rocTop, rocWidth, findScroll, percentScroll, per;
 
   var i = 0;
   
   for(; i < 1 ; i++){
     rocTop = rocket.offset().top;
+    rocWidth = parseInt(rocket.offset().width);
     // 선택된 형태가 브라우저 하단에서 0부터 값 나오도록
     findScroll = winScroll - rocTop + winH;
     // % 계산법 : 스크롤값 / 기준치 * 100
     percentScroll = parseInt(findScroll / winH * 100);
     per = percentScroll;
     if (percentScroll < 0){ per = 0;} else if (percentScroll > 100){ per = 100; }
-    rocket.stop().animate({marginTop: -per/2 + 'px', marginLeft: per + '%'}, 1000 );
-    // rocket.css({transform:'translateY(' + -per + '%)', transition:'all 1000ms linear'});
+    rocket.stop().animate({marginTop: -per/3 + 'px'}, 300 );
+    // rocket.css({transform:'translate(' + -per + '%)', transition:'all 1000ms linear'});
   }
   
 
