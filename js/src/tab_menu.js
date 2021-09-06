d51afe3e9b6e6d8837af1899a6dc1152e674ec5e
Sejeong Kim <83333195+sejeongit@users.noreply.github.com>
2021-09-06 17:09:25 +0900

    resume -> about me 변경, 탭메뉴 추가

---
+++ js/src/tab_menu.js
@@ -0,0 +1,28 @@
+// tab_menu.js
+(function($){
+// jQuery
+
+var tabUl = $('.skill_category');
+var tabLi = tabUl.children('li');
+var tabBtn = tabLi.children('button');
+var tabContent = $('.con');
+
+tabLi.eq(0).addClass('act');
+
+tabBtn.on('focus', function(e){
+  e.preventDefault();
+
+  var _this = $(this);
+  var _thisParent = _this.parent('li');
+  var _i = _thisParent.index();
+
+  var liEq = tabLi.eq( _i );
+  liEq.addClass('act');
+  liEq.siblings().removeClass('act');
+  var conEq = tabContent.eq( _i );
+  conEq.show();
+  conEq.siblings().hide();
+}); // tabBtn.on('focus')
+
+
+})(jQuery);
