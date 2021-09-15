089d1b6094d009527ea5de64cdb83a583b93f15a
Sejeong Kim <83333195+sejeongit@users.noreply.github.com>
2021-09-15 16:08:39 +0900

    web portfolio 반응형 페이지 제작 외

--- js/src/rocket_parallax.js
+++ js/src/rocket_parallax.js
@@ -17,13 +17,12 @@ var rocket = intro.find('.rocket');
 win.on('scroll', function(e){
   var winScroll = win.scrollTop();
   var winH = win.outerHeight();
-  var rocTop, rocWidth, findScroll, percentScroll, per;
+  var rocTop, findScroll, percentScroll, per;
 
   var i = 0;
   
   for(; i < 1 ; i++){
     rocTop = rocket.offset().top;
-    rocWidth = parseInt(rocket.offset().width);
     // 선택된 형태가 브라우저 하단에서 0부터 값 나오도록
     findScroll = winScroll - rocTop + winH;
     // % 계산법 : 스크롤값 / 기준치 * 100
--- js/src/tab_menu.js
+++ js/src/tab_menu.js
@@ -25,5 +25,21 @@ tabBtn.on('focus', function(e){
   conEq.siblings().hide();
 }); // tabBtn.on('focus')
 
+tabBtn.on('click', function(e){
+  e.preventDefault();
+
+  var _this = $(this);
+  var _thisParent = _this.parent('li');
+  var _i = _thisParent.index();
+
+  var liEq = tabLi.eq( _i );
+  liEq.addClass('act');
+  liEq.siblings().removeClass('act');
+  
+  var conEq = tabContent.eq( _i );
+  conEq.show();
+  conEq.siblings().hide();
+}); // tabBtn.on('click')
+
 
 })(jQuery);
